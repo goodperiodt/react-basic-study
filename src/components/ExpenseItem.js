0b21f9c73028c1108bb784e6cfa7bbff605dc905
goodperiodt <ilypsj@gmail.com>
2024-05-23 22:19:51 +0900

    4. useState 훅으로 상태변수 값 변경하고, 화면에 렌더링하기 (수정 버튼)

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "./ui/Card";
@@ -54,6 +54,28 @@ const ExpenseItem = ({ title, price: p, date }) => {
   }*/
   ).format(p);
 
+  /*
+    값이 변경되어 화면에 반영되어야 하는 값들은
+    useState 훅을 통해 상태변수로 관리해야 한다.
+
+    훅은 리액트에서 직접 코드 작성없이 다양한 기능을 사용할 수 있게 도와주는 라이브러리
+    리액트 자체에서 제공하는 라이브러리를 훅이라고 한다.
+
+
+  */
+  const [itemTitle, setTitle] = useState(title); // 1. useState의 매개 값으로 상태변수의 초기 값을 전달한다.
+  // 2. useState() 는 배열을 반환하고, 배열의 첫 번째 요소는 관리할 상태 값(상태변수)
+  // 배열의 두 번째 요소는 상태변수의 값을 변경하는 setter함수가 리턴된다.
+  // 상태변수의 값을 변경할 때는 반드시 setter함수를 호출해서 변경해야 한다.
+  // 대입으로는 상태변수의 값이 변경되지 않는다.
+
+  const clickHandler = (e) => {
+    console.log(itemTitle);
+    // state로 관리하는 변수(상태변수)는 반드시 setter로만 변경해야 한다.
+    setTitle("메롱메롱");
+  };
+  console.log(itemTitle);
+
   return (
     // Card 컴포넌트가 받게될 클래스명은 "circle" 이다.
     <Card className="circle">
@@ -62,10 +84,15 @@ const ExpenseItem = ({ title, price: p, date }) => {
         {/* <div>{makeFormattedDate()}</div> --> ExpenseDate 컴포넌트로 생성*/}
         <ExpenseDate date={date} />
         <div className="expense-item__description">
-          <h2>{title}</h2>
+          <h2 id="title">{itemTitle}</h2>
           <div className="expense-item__price">{formattedPrice}원</div>
         </div>
       </div>
+      {/* 수정 버튼에 클릭이 발생했을 때, clickHandler 함수를 호출한다. */}
+      <button id="btn1" onClick={clickHandler}>
+        수정
+      </button>
+      <button id="btn2">삭제</button>
     </Card>
   );
 };
