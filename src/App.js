18c0a73810c9fb2b4910a05c84153f0de045d94f
goodperiodt <ilypsj@gmail.com>
2024-06-05 09:16:22 +0900

    상태변수 변화로 무한 리렌더링되던 문제 해결 --> useEffect() 함수 사용

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 // import AddUsers from "./components/Users/AddUsers";
 // import UserList from "./components/Users/UserList";
 import "./App.css";
@@ -7,16 +7,30 @@ import Login from "./components/SideEffect/Login/Login";
 import Home from "./components/SideEffect/Home/Home";
 
 const App = () => {
+  console.log("App 컴포넌트 수행");
   // 로그인 상태를 관리하는 변수
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   // 화면이 리렌더링될 때 LocalStorage를 확인해서
   // 현재 Login-flag가 존재하는지 검사
   console.log("로그인 검사 수행!");
+
+  /* 무한 리렌더링되는 문제가 되는 코드
   const storedLoginFlag = localStorage.getItem("login-flag");
   if (storedLoginFlag === "1") {
     setIsLoggedIn(true);
   }
+  */
+  // 기존에 로그인 한 사람인지 확인하는 코드는
+  // 리렌더링될 때마다 실행하면 안됨! (한 번만 확인하면 됨)
+  useEffect(() => {
+    console.log("useEffect 실행! - 최초 단 한번만 실행");
+    const storedLoginFlag = localStorage.getItem("login-flag");
+    if (storedLoginFlag === "1") {
+      setIsLoggedIn(true);
+    }
+  }, []);
+  // useEffect() 함수의 두 번째 파라미터로 받는 배열은 의존성 배열이다. 배열 안에 변수를 지정하면, 해당 변수의 값이 변할 때마다 useEffect()가 실행된다. 만약 배열을 비워놓으면, 렌더링 과정에서 단 한 번만 실행된다.
 
   // 서버로 로그인을 요청하는 함수. (나중에는 fetch를 통한 백엔드와의 연계가 필요.)
   const loginHandler = (email, password) => {
@@ -26,6 +40,7 @@ const App = () => {
     // 상태변수(isLoggedIn)에 변화가 있다면 리액트는 다시 렌더링을 한다.
     setIsLoggedIn(true);
   };
+  console.log("App 컴포넌트의 끝");
   return (
     <>
       <MainHeader />
